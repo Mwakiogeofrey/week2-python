6e41bc156c869484db8894a0350ea4139d4efd2e
Mwakiogeofrey <mwakiogeofrey579@gmail.com>
2025-03-31 12:26:17 +0300

    Add python data list

---
+++ main.py
@@ -0,0 +1,13 @@
+my_List = []
+
+my_List.extend ([10, 20, 30, 40 ])
+
+my_List.insert(1, 15)
+
+my_List.extend([50, 60, 70])
+
+my_List.pop()
+my_List.sort()
+
+index_30 = my_List.index(30)
+print("Index of 30:", index_30)
